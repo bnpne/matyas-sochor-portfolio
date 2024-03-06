e5e53fda591641d3a3739e6216bb7f0e397f5dea
Ben Paine <bentppaine@gmail.com>
2024-03-06 01:27:08 -0800

    nuxi

--- www/nuxt.config.ts
+++ www/nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   devtools: {enabled: false},
   pages: true,
   modules: ['@nuxtjs/sanity'],
+  build: {
+    transpile: ['sanity'],
+  },
   css: ['~/assets/styles/globals.scss'],
   sanity: {
     projectId: process.env.SANITY_PROJECT_ID,
